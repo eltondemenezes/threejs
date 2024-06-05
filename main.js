ee8f12eaa56e2a050f238f4bd0d6b3380ea58337
Eltondemenezes <67955607+eltondemenezes@users.noreply.github.com>
2024-06-05 22:19:19 +0100

    three basics

--- main.js
+++ main.js
@@ -1,24 +1,67 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: '#00ff83'
+})
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-setupCounter(document.querySelector('#counter'))
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+const light = new THREE.PointLight(0xffffff, 100, 100);
+light.position.set(0, 10, 10);
+scene.add(light)
+
+const camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.1, 100);
+camera.position.z = 20
+scene.add(camera);
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(2)
+renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true
+controls.autoRotate = true
+
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  camera.aspect = size.width / size.height;
+  renderer.setSize(size.width, size.height);
+  camera.updateProjectionMatrix()
+})
+
+const loop = () => {
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop)
+}
+loop();
+
+
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / size.width) * 255),
+      Math.round((e.pageY / size.width) * 255),
+      150
+    ]
+    let newColor = new THREE.Color(`rgb(${rgb.join(',')})`)
+    mesh.material.color = newColor
+  }
+})
